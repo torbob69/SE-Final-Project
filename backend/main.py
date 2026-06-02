0779ad339ef447a69c136f5f1505c8d39dcda9f7
torbob <nicholaswilson7803@gmail.com>
2026-06-02 21:14:28 +0700

    fixed cors issue; added vercel url

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="StockSync API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://stock-sync-tan.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
